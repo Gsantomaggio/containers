39d644fbcf0c0ca812ffa85a8bca999a5d21164a
Gabriele Santomaggio <g.santomaggio@gmail.com>
2024-09-05 22:13:11 +0200

    EBPF firewall
    Signed-off-by: Gabriele Santomaggio <g.santomaggio@gmail.com>

---
+++ ebpf_ns_firewall/kernel_module/xdp_tcp.c
@@ -0,0 +1,67 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+//#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_endian.h>
+
+#define IPPROTO_TCP  6
+#define ETH_P_IP  0x0800
+
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __type(key, u32);
+    __type(value, u64);
+    __uint(max_entries, 1);
+} port_filter SEC(".maps");
+
+SEC("xdp") int xdp_tcp_firewall(struct xdp_md *ctx) {
+    void *data_end = (void *) (long) ctx->data_end;
+        void *data = (void *) (long) ctx->data;
+
+        struct ethhdr *eth = data;
+        if ((void *) (eth + 1) > data_end) {
+            return XDP_PASS;
+        }
+
+        struct iphdr *iph = data + sizeof(struct ethhdr);
+        if ((void *) (iph + 1) > data_end) {
+            return XDP_PASS;
+        }
+
+        if (iph->protocol != IPPROTO_TCP) {
+            return XDP_PASS;
+        }
+
+        struct tcphdr *tcp = data + sizeof(struct ethhdr) + sizeof(struct iphdr);
+        if ((void *) (tcp + 1) > data_end) {
+            return XDP_PASS;
+        }
+
+
+    u32 key = 0;
+    u64 *value;
+    value = bpf_map_lookup_elem(&port_filter, &key);
+//    bpf_printk("[EBPF Kernel Space VALUE]  value is %d  \n", value);
+
+    if (value == NULL) {
+        bpf_printk("[EBPF Kernel Space VALUE]  value is NULL \n");
+    } else {
+
+        if (bpf_htons(tcp->dest) == 22) {
+            return XDP_PASS;
+        }
+        if (bpf_htons(tcp->dest) != *value) {
+            return XDP_PASS;
+        } else {
+             bpf_printk("[ebpf firewall] packets to port: %d will be dropped. \n", *value);
+            return XDP_DROP;
+        }
+
+
+        }
+         return XDP_PASS;
+ }
+
+
+ char _license[]
+         SEC("license") = "GPL";
---
+++ ebpf_ns_firewall/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/rlimit"
+	"log/slog"
+	"net"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	const networkInterface = "veth-red"
+	const XdpTcpObj = "./kernel_module/xdp_tcp.o"
+
+	slogOpts := &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}
+	log := slog.New(slog.NewTextHandler(os.Stdout, slogOpts))
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Error("failed to remove memlock: %v", err)
+	}
+
+	spec, err := ebpf.LoadCollectionSpec(XdpTcpObj)
+	if err != nil {
+		log.Error("Error loading eBPF object file", "file", XdpTcpObj, "error", err)
+		return
+	}
+
+	coll, err := ebpf.NewCollection(spec)
+	if err != nil {
+		log.Error("Error creating eBPF collection", "error", err)
+		return
+	}
+	log.Info("eBPF object file loaded. ", "file", XdpTcpObj)
+
+	ifce, err := net.InterfaceByName(networkInterface)
+	if err != nil {
+		log.Error("failed to get interface: %v", err)
+	}
+
+	log.Info("interfaces found", "interface", ifce.Name, "index", ifce.Index)
+
+	l, err := link.AttachXDP(link.XDPOptions{
+		Program:   coll.Programs["xdp_tcp_firewall"],
+		Interface: ifce.Index,
+	})
+	if err != nil {
+		log.Error("error", "failed to attach XDP program", "err", err)
+	}
+
+	var value2 uint64
+	value2 = 5552
+
+	if err := coll.Maps["port_filter"].Put(uint32(0), &value2); err != nil {
+		log.Error("failed to lookup map: %v", err)
+	}
+
+	log.Info("XDP firewall attached: ", "interface", ifce.Name, "port", value2)
+
+	defer l.Close()
+
+	<-sig
+
+}
